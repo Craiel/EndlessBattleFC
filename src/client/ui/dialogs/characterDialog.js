519b7ffe7ee9a9e4f87962b7afd1a5eb2dfc90af
Craiel <loft45@gmail.com>
2015-03-16 10:07:58 -0400

    Character Stat display - stats for player are now displayed in categorized list

--- src/client/ui/dialogs/characterDialog.js
+++ src/client/ui/dialogs/characterDialog.js
@@ -41,6 +41,12 @@ declare('CharacterDialog', function() {
         this.trinket2Slot = undefined;
 
         this.characterInventory = undefined;
+
+        this.statCategoryHeaders = {};
+        this.statElements = {};
+
+        this.statUpdateTime = 0;
+        this.statUpdateDelay = 250;
     };
 
     // ---------------------------------------------------------------------------
@@ -61,6 +67,7 @@ declare('CharacterDialog', function() {
         this.statsBackground.init(this.getContentArea());
         resources.setPanelImages(this.statsBackground, "BeigeInset");
         this.statsBackground.addClass('characterStatsBackground');
+        this.statsBackground.getContentArea().setStyle({"overflow-y": "scroll"});
 
         this.inventoryBackground = panel.create(this.id + 'InventoryBackground');
         this.inventoryBackground.init(this.getContentArea());
@@ -72,6 +79,8 @@ declare('CharacterDialog', function() {
         this.characterInventory = inventoryControl.create("characterInventory");
         this.characterInventory.storage = game.player.storage;
         this.characterInventory.init(this.inventoryBackground.getContentArea());
+
+        this.initStats();
 
         this.hide();
     };
@@ -87,6 +96,17 @@ declare('CharacterDialog', function() {
         }
 
         // Update the child components if the window is visible
+        this.updateInventory(gameTime);
+        this.updateStats(gameTime);
+
+        return true;
+    }
+
+    // ---------------------------------------------------------------------------
+    // dialog functions
+    // ---------------------------------------------------------------------------
+    CharacterDialog.prototype.updateInventory = function(gameTime) {
+
         this.characterInventory.update(gameTime);
 
         // Make the inventory scrollable if there are too many slots (Temporary)
@@ -95,13 +115,84 @@ declare('CharacterDialog', function() {
         } else {
             this.inventoryBackground.getContentArea().setStyle({"overflow-y": "none"});
         }
-
-        return true;
     }
 
-    // ---------------------------------------------------------------------------
-    // dialog functions
-    // ---------------------------------------------------------------------------
+    CharacterDialog.prototype.updateStats = function(gameTime) {
+        if(this.statUpdateTime === 0) {
+            this.statUpdateTime = gameTime.current + this.statUpdateDelay;
+            return;
+        }
+
+        if(gameTime.current < this.statUpdateTime) {
+            return;
+        }
+
+        for(var category in this.statElements) {
+            for(var i = 0; i < this.statElements[category].length; i++) {
+                var statKey = this.statElements[category][i][0];
+                var element = this.statElements[category][i][2];
+                var stat = data.StatDefinition[statKey];
+                var value = game.player.getStat(stat.id);
+                if(stat.isMultiplier === true) {
+                    value = Math.floor((value - 1) * 100);
+                }
+
+                element.setText(stat.displayValue.format(value));
+            }
+        }
+
+        this.statUpdateTime = gameTime.current + this.statUpdateDelay;
+    }
+
+    CharacterDialog.prototype.initStats = function(gameTime) {
+        var categories = {};
+        for(var key in data.StatDefinition) {
+            var stat = data.StatDefinition[key];
+            if(stat.displayCategory === undefined) {
+                continue;
+            }
+
+            if(categories[stat.displayCategory] === undefined) {
+                categories[stat.displayCategory] = [];
+            }
+
+            categories[stat.displayCategory].push(stat);
+        }
+
+        for(var category in categories) {
+            console.log(categories[category]);
+            var categoryHeader = element.create("playerStatCategory_" + category);
+            categoryHeader.templateName = "characterStatDisplayCategory";
+            categoryHeader.init(this.statsBackground.getContentArea());
+            categoryHeader.setText(category);
+            this.statCategoryHeaders[category] = categoryHeader;
+
+            for(var i = 0; i < categories[category].length; i++) {
+                var stat = categories[category][i];
+
+                var characterStatLine = element.create("playerStat" + stat.id);
+                characterStatLine.templateName = "characterStatDisplayLine";
+                characterStatLine.init(this.statsBackground.getContentArea());
+
+                var statElement = element.create("playerStatValue" + stat.id);
+                statElement.templateName = "characterStatDisplayValue";
+                statElement.init(characterStatLine);
+                statElement.setText(stat.displayValue);
+
+                var statHeader = element.create("playerStatHeader" + stat.id);
+                statHeader.templateName = "characterStatDisplayHeader";
+                statHeader.init(characterStatLine);
+                statHeader.setText(stat.displayHeader);
+
+                if(this.statElements[category] === undefined) {
+                    this.statElements[category] = [];
+                }
+
+                this.statElements[category].push([stat.id, statHeader, statElement]);
+            }
+        }
+    }
+
     CharacterDialog.prototype.initEquipmentSlots = function() {
         this.headSlot = this.createEquipSlot('equipSlotHead');
         this.chestSlot = this.createEquipSlot('equipSlotChest');
